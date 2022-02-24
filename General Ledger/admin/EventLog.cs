490cab5b40ccaeab36c1ff3738a665c4cbf18fb0
michaeljw5 <michaelwoodward94@gmail.com>
2022-02-24 00:40:56 -0500

    Revert "Revert "Pro: have set up all screens and I believe I have set up the input account to specification.""
    This reverts commit ed97b9ccab6ad7bbcb4b355095f223ef08916530.

---
+++ General Ledger/admin/EventLog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace General_Ledger.admin
+{
+    public partial class EventLog : Form
+    {
+        public EventLog()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
